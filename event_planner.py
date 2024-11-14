fbdb4088398af227de6fdc4f4017e2df6fd63ae5
Rémi <remi.peillet@aladom.fr>
2024-11-14 10:21:39 +0100

    Add comments

--- event_planner.py
+++ event_planner.py
@@ -21,19 +21,36 @@ class EventPlanner:
     def __init__(self):
         self.events = []
 
-    def add_event(self, name, start_time, end_time):
+    '''
+        Method for adding events to the planner
+        Params:
+            name: <str> name of the event
+            start_time: <time> or <str> with '%H:%M' format start time of 
+            the event
+            end_time: <time> or <str> with '%H:%M' format end time of the event
+        Returns:
+            None
+        Name must be unique.
+        Start time must be upper than end time.
+        If conflict is detected we ask if we would force insert it or not by 
+        enter 'o' in console input
+    '''
+    def add_event(self, name, start_time, end_time) -> None:
         if isinstance(start_time, str):
             start_time = datetime.strptime(start_time, "%H:%M").time()
         elif not isinstance(start_time, time):
             raise ValueError(
-                "l'heure de début doit être un objet de type datetime.time ou str")
+                "l'heure de début doit être un objet de type datetime.time ou "
+                "str")
         if isinstance(end_time, str):
             end_time = datetime.strptime(end_time, "%H:%M").time()
         elif not isinstance(start_time, time):
             raise ValueError(
-                "l'heure de fin doit être un objet de type datetime.time ou str")
+                "l'heure de fin doit être un objet de type datetime.time ou "
+                "str")
         if start_time > end_time:
-            raise ValueError("l'heure de début doit être inférieur à l'heure de fin")
+            raise ValueError("l'heure de début doit être inférieur à "
+                             "l'heure de fin")
         new_event = Event(name, start_time, end_time)
         conflicts = []
         for event in self.events:
@@ -49,10 +66,24 @@ class EventPlanner:
         else:
             self.events.append(new_event)
 
-    def list_events(self):
+    """
+        List events of planner order by start time
+        Params:
+            None
+        Returns:
+            <List[Event]> list of events sorted by start time
+    """
+    def list_events(self) -> List[Event]:
         return sorted(self.events, key=lambda event: event.start_time)
 
-    def find_conflicts(self):
+    """
+        Find conflicts in planner
+        Params:
+            None
+        Returns:
+            <List[tuple[Event, Event]]> list of conflicts
+    """
+    def find_conflicts(self) -> List[tuple[Event, Event]]:
         conflicts = []
         for index, event in enumerate(self.list_events()):
             for next_event in self.list_events()[index + 1:]:
